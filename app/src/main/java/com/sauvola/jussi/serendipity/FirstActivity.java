45acbb4d408f6d1235258664b4cfb1b5fa159342
Silvergrail <ulfest@hotmail.com>
2016-02-24 19:48:18 +0200

    Merged Yuqing branch and added functionality for new views

--- app/src/main/java/com/sauvola/jussi/serendipity/FirstActivity.java
+++ app/src/main/java/com/sauvola/jussi/serendipity/FirstActivity.java
@@ -1,11 +1,13 @@
 package com.sauvola.jussi.serendipity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -13,6 +15,33 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        Button mLoginButton = (Button) findViewById(R.id.login_button);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.sauvola.jussi.serendipity.LoginActivity");
+                startActivity(intent);
+            }
+        });
+
+        Button mRegisterButton = (Button) findViewById(R.id.register_button);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.sauvola.jussi.serendipity.RegisterActivity");
+                startActivity(intent);
+            }
+        });
+
+        Button mMapButton = (Button) findViewById(R.id.map_button);
+        mMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.sauvola.jussi.serendipity.MapsActivity");
+                startActivity(intent);
+            }
+        });
 
     }
 
