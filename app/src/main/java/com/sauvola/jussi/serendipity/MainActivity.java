5ca98f75291e7d868b7b089d79146aada311aef2
Silvergrail <ulfest@hotmail.com>
2016-02-25 19:48:17 +0200

    Altered the login function to match the PHP queries in the server but it's not yet working. JSON seems to be the problem

---
+++ app/src/main/java/com/sauvola/jussi/serendipity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sauvola.jussi.serendipity;
+
+/**
+ * Created by Juze on 25.2.2016.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button logout_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logout_button = (Button) findViewById(R.id.logout_button);
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+    }
+}
