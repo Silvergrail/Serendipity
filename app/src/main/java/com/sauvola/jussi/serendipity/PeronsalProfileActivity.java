26358714534102e960c190f1652ae32d6121ff08
Yuqing Wang <465577079@qq.com>
2016-03-06 06:19:02 +0800

    Create UI for "record file details" and "personal profile"

---
+++ app/src/main/java/com/sauvola/jussi/serendipity/PeronsalProfileActivity.java
@@ -0,0 +1,13 @@
+package com.sauvola.jussi.serendipity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PeronsalProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peronsal_profile);
+    }
+}
