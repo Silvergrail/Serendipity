abea3dd973de1146959e146030a1b38392a28cc7
Silvergrail <ulfest@hotmail.com>
2016-03-13 23:06:51 +0200

    Added UI elements and added the recording functionality to them

--- app/src/main/java/com/sauvola/jussi/serendipity/ProfileActivity.java
+++ app/src/main/java/com/sauvola/jussi/serendipity/ProfileActivity.java
@@ -1,13 +1,32 @@
 package com.sauvola.jussi.serendipity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button record_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        record_button = (Button) findViewById(R.id.record_button);
+
+        record_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent("com.sauvola.jussi.serendipity.RecordActivity");
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/sauvola/jussi/serendipity/RecordActivity.java
+++ app/src/main/java/com/sauvola/jussi/serendipity/RecordActivity.java
@@ -1,13 +1,174 @@
 package com.sauvola.jussi.serendipity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.io.IOException;
 
-public class RecordActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.widget.Button;
+import android.media.MediaPlayer;
+
+public class RecordActivity extends Activity {
+
+
+    private static MediaRecorder mediaRecorder;
+    private static MediaPlayer mediaPlayer;
+
+    private static final String LOG_TAG = "AudioRecordTest";
+
+    private static String mFileName;
+    private static Button stopButton;
+    private static Button playButton;
+    private static Button recordButton;
+
+    private boolean isRecording = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_record);
+
+        recordButton = (Button) findViewById(R.id.start_record_button);
+        playButton = (Button) findViewById(R.id.record_cancel_btn);
+        stopButton = (Button) findViewById(R.id.record_pause_btn);
+
+
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isRecording = true;
+                stopButton.setEnabled(true);
+                playButton.setEnabled(false);
+                recordButton.setEnabled(false);
+
+                try {
+                    mediaRecorder = new MediaRecorder();
+                    mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                    mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                    mediaRecorder.setOutputFile(mFileName);
+                    mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                    mediaRecorder.prepare();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                mediaRecorder.start();
+
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopButton.setEnabled(false);
+                playButton.setEnabled(true);
+
+                if (isRecording)
+                {
+                    recordButton.setEnabled(false);
+                    mediaRecorder.stop();
+                    mediaRecorder.release();
+                    mediaRecorder = null;
+                    isRecording = false;
+                } else {
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                    recordButton.setEnabled(true);
+                }
+
+            }
+            });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                playButton.setEnabled(false);
+                recordButton.setEnabled(false);
+                stopButton.setEnabled(true);
+
+                mediaPlayer = new MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(mFileName);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "prepare() failed");
+                }
+
+            }
+            });
+
     }
+
+    public void recordAudio (View view) throws IOException
+    {
+        isRecording = true;
+        stopButton.setEnabled(true);
+        playButton.setEnabled(false);
+        recordButton.setEnabled(false);
+
+        try {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setOutputFile(mFileName);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.prepare();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mediaRecorder.start();
+    }
+
+    public void stopClicked (View view)
+    {
+
+        stopButton.setEnabled(false);
+        playButton.setEnabled(true);
+
+        if (isRecording)
+        {
+            recordButton.setEnabled(false);
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            isRecording = false;
+        } else {
+            mediaPlayer.release();
+            mediaPlayer = null;
+            recordButton.setEnabled(true);
+        }
+    }
+
+    public void playAudio (View view) throws IOException
+    {
+        playButton.setEnabled(false);
+        recordButton.setEnabled(false);
+        stopButton.setEnabled(true);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setDataSource(mFileName);
+        mediaPlayer.prepare();
+        mediaPlayer.start();
+    }
+
+
+
 }
