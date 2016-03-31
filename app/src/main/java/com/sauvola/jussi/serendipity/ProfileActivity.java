dadf01b4e6ccaac99237a75aa802d8294dfc8b52
Silvergrail <ulfest@hotmail.com>
2016-03-31 22:52:24 +0200

    Added Sharedpreferences but they require work

--- app/src/main/java/com/sauvola/jussi/serendipity/ProfileActivity.java
+++ app/src/main/java/com/sauvola/jussi/serendipity/ProfileActivity.java
@@ -12,6 +12,9 @@ import android.widget.SimpleAdapter;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
@@ -20,6 +23,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -29,19 +33,11 @@ import java.util.HashMap;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    String myJSON;
+    //SessionManager session;
 
-    private static final String TAG_RESULTS="result";
-    private static final String FILE_ID = "file_id";
-    private static final String FILE_TITLE = "file_title";
-    private static final String FILE_DESCRIPTION ="file_description";
-    private static final String FILE_GPS ="GPS";
+    String JSONOutput = "";
 
     Button recordButton;
-
-    JSONArray musics = null;
-
-    ArrayList<HashMap<String, String>> musicList;
 
     ListView list;
 
@@ -49,6 +45,8 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //connectWithHttpGet(session.getUserID());
 
         recordButton = (Button) findViewById(R.id.record_button);
 
@@ -61,96 +59,78 @@ public class ProfileActivity extends AppCompatActivity {
 
         });
 
+
         list = (ListView) findViewById(R.id.listView);
-        musicList = new ArrayList<HashMap<String,String>>();
-        getData();
-    }
 
-
-    protected void showList(){
-        try {
-            JSONObject jsonObj = new JSONObject(myJSON);
-            musics = jsonObj.getJSONArray(TAG_RESULTS);
-
-            for(int i=0;i< musics.length();i++){
-                JSONObject c = musics.getJSONObject(i);
-                String file_id = c.getString(FILE_ID);
-                String file_title = c.getString(FILE_TITLE);
-                String file_description = c.getString(FILE_DESCRIPTION);
-                String file_gps = c.getString(FILE_GPS);
-
-
-                HashMap<String,String> musics = new HashMap<String,String>();
-
-                musics.put(FILE_ID, file_id);
-                musics.put(FILE_TITLE, file_title);
-                musics.put(FILE_DESCRIPTION, file_description);
-                musics.put(FILE_GPS, file_gps);
-
-
-                musicList.add(musics);
-            }
-
-            ListAdapter adapter = new SimpleAdapter(
-                    ProfileActivity.this, musicList, R.layout.list_item,
-                    new String[]{FILE_ID, FILE_TITLE, FILE_DESCRIPTION, FILE_GPS},
-                    new int[]{R.id.file_id, R.id.file_title, R.id.file_description, R.id.file_gps}
-            );
-
-            list.setAdapter(adapter);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
     }
 
-    public void getData(){
-        class GetDataJSON extends AsyncTask<String, Void, String>{
+    private void connectWithHttpGet(String id) {
+
+        class HttpGetAsyncTask extends AsyncTask<String, Void, String>{
 
             @Override
             protected String doInBackground(String... params) {
-                DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
-                HttpPost httppost = new HttpPost("http://www.serendipitydemo.com/getmusic.php");
 
-                // Depends on your web service
-                // httppost.setHeader("Content-type", "application/json");
+                String ParamID = params[0];
+                System.out.println("id" + ParamID);
 
-                InputStream inputStream = null;
-                String result = null;
+                // Create an intermediate to connect with the Internet
+                HttpClient httpClient = new DefaultHttpClient();
+
+                HttpGet httpGet = new HttpGet("http://serendipitydemo.com/getmusic.php?id=" + ParamID);
+
                 try {
-                    HttpResponse response = httpclient.execute(httppost);
-                    HttpEntity entity = response.getEntity();
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    InputStream inputStream;
 
-                    inputStream = entity.getContent();
-                    // json is UTF-8 by default
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
-                    StringBuilder sb = new StringBuilder();
+                    inputStream = httpResponse.getEntity().getContent();
 
-                    String line = null;
-                    while ((line = reader.readLine()) != null)
-                    {
-                        sb.append(line + "\n");
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    String bufferedStrChunk = null;
+
+                    while((bufferedStrChunk = bufferedReader.readLine()) != null){
+                        stringBuilder.append(bufferedStrChunk);
                     }
-                    result = sb.toString();
-                } catch (Exception e) {
-                    // Oops
+
+                    return stringBuilder.toString();
+
+                } catch (ClientProtocolException cpe) {
+                    cpe.printStackTrace();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
                 }
-                finally {
-                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
-                }
-                return result;
+
+                return null;
             }
 
             @Override
-            protected void onPostExecute(String result){
-                myJSON=result;
-                showList();
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                JSONOutput = result;
+
+/*                if(result.equals("Joomla! Authentication was successful!")){
+                    Toast.makeText(getApplicationContext(), "Welcome " + JSONOutput, Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent("com.sauvola.jussi.serendipity.ProfileActivity");
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Username or password is invalid", Toast.LENGTH_LONG).show();
+                }*/
+
             }
         }
-        GetDataJSON g = new GetDataJSON();
-        g.execute();
+
+        HttpGetAsyncTask httpGetAsyncTask = new HttpGetAsyncTask();
+        httpGetAsyncTask.execute(id);
+
     }
+
+
 
 
 }
