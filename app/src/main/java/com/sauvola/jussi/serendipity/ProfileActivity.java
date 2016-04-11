a3662105a54e732b3f5a357a2ea466d6bb417526
Yuqing Wang <465577079@qq.com>
2016-04-12 03:36:21 +0800

    fix bugs on event handler
    It should be ‘findViewById(R.id.SettingButton) ‘ rather than
    ‘findViewById(R.id.register_button);’

--- app/src/main/java/com/sauvola/jussi/serendipity/ProfileActivity.java
+++ app/src/main/java/com/sauvola/jussi/serendipity/ProfileActivity.java
@@ -87,7 +87,7 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button mSettingButton = (Button) findViewById(R.id.register_button);
+        Button mSettingButton = (Button) findViewById(R.id.SettingButton);
         mSettingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -113,9 +113,9 @@ public class ProfileActivity extends AppCompatActivity {
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent intent = new Intent("com.sauvola.jussi.serendipity.RecordActivity");
-                    startActivity(intent);
-                }
+                Intent intent = new Intent("com.sauvola.jussi.serendipity.RecordActivity");
+                startActivity(intent);
+            }
 
         });
 
