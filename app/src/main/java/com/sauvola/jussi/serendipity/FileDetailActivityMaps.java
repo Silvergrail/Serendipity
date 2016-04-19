7426484e79b655b371b9728c80af8c5f592646bc
Silvergrail <ulfest@hotmail.com>
2016-04-19 18:04:36 +0200

    The stream player from the server works! Minor UI difficulties though

--- app/src/main/java/com/sauvola/jussi/serendipity/FileDetailActivityMaps.java
+++ app/src/main/java/com/sauvola/jussi/serendipity/FileDetailActivityMaps.java
@@ -7,7 +7,11 @@ package com.sauvola.jussi.serendipity;
 import static com.sauvola.jussi.serendipity.MapsActivity.TAG_FILE_TITLE;
 import static com.sauvola.jussi.serendipity.MapsActivity.TAG_FILE_GPS;
 import static com.sauvola.jussi.serendipity.MapsActivity.TAG_FILE_DESCRIPTION;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +20,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -34,27 +39,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import android.media.MediaPlayer;
 
 public class FileDetailActivityMaps extends AppCompatActivity {
 
     public static final String TAG_FILE_DESCRIPTION = "description";
     public static final String TAG_FILE_GPS = "GPS";
+    public static final String TAG_FILE_SOURCE = "url_download";
+    public static String audioSource = "";
+
+    MediaPlayer mPlayer = new MediaPlayer();
+
+    private boolean intialStage = true;
+    private boolean playPause;
 
     Button deleteButton;
     Button saveButton;
+    ImageButton playButton;
     private String JSONOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_detail);
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);;
 
         Intent in = getIntent();
 
         String file_title = "";
         String file_gps = "";
         String description = "";
+
 
         if (null != in) {
             file_title = in.getStringExtra(TAG_FILE_TITLE);
@@ -67,8 +82,38 @@ public class FileDetailActivityMaps extends AppCompatActivity {
 
         saveButton = (Button) findViewById(R.id.save_btn);
 
+        playButton = (ImageButton) findViewById(R.id.record_play_button);
+
         connectWithHttpGet(file_title);
+
+        playButton.setOnClickListener(pausePlay);
+
     }
+
+    private View.OnClickListener pausePlay = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+
+            if (!playPause) {
+                playButton.setBackgroundResource(R.drawable.ic_record_pause);
+                if (intialStage)
+                    new Player()
+                            .execute("http://www.serendipitydemo.com/jdownloads/_uncategorised_files/" + audioSource);
+                else {
+                    if (!mPlayer.isPlaying())
+                        mPlayer.start();
+                }
+                playPause = true;
+            } else {
+                playButton.setBackgroundResource(R.drawable.ic_record_play);
+                if (mPlayer.isPlaying())
+                    mPlayer.pause();
+                playPause = false;
+            }
+        }
+    };
 
     private void connectWithHttpGet(String file_title) {
 
@@ -100,7 +145,7 @@ public class FileDetailActivityMaps extends AppCompatActivity {
 
                     String bufferedStrChunk = null;
 
-                    while((bufferedStrChunk = bufferedReader.readLine()) != null){
+                    while ((bufferedStrChunk = bufferedReader.readLine()) != null) {
                         stringBuilder.append(bufferedStrChunk);
                     }
 
@@ -133,6 +178,7 @@ public class FileDetailActivityMaps extends AppCompatActivity {
                         String file_gps = m.getString(TAG_FILE_GPS);
                         TextView file_gps_text = (TextView) findViewById(R.id.file_gps_text);
                         file_gps_text.setText(file_gps);
+                        audioSource = m.getString(TAG_FILE_SOURCE);
                     }
 
                 } catch (JSONException e) {
@@ -147,4 +193,81 @@ public class FileDetailActivityMaps extends AppCompatActivity {
         httpGetAsyncTask.execute(file_title);
 
     }
-}
+
+    class Player extends AsyncTask<String, Void, Boolean> {
+        private ProgressDialog progress;
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            Boolean prepared;
+            try {
+
+                mPlayer.setDataSource(params[0]);
+
+                mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        intialStage = true;
+                        playPause = false;
+                        playButton.setBackgroundResource(R.drawable.ic_record_play);
+                        mPlayer.stop();
+                        mPlayer.reset();
+                    }
+                });
+                mPlayer.prepare();
+                prepared = true;
+            } catch (IllegalArgumentException e) {
+                Log.d("IllegarArgument", e.getMessage());
+                prepared = false;
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                prepared = false;
+                e.printStackTrace();
+            } catch (IllegalStateException e) {
+                prepared = false;
+                e.printStackTrace();
+            } catch (IOException e) {
+                prepared = false;
+                e.printStackTrace();
+            }
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+            if (progress.isShowing()) {
+                progress.cancel();
+            }
+            Log.d("Prepared", "//" + result);
+            mPlayer.start();
+
+            intialStage = false;
+        }
+
+        public Player() {
+            progress = new ProgressDialog(FileDetailActivityMaps.this);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            this.progress.setMessage("Buffering...");
+            this.progress.show();
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mPlayer != null) {
+            mPlayer.reset();
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+    }
+
+
